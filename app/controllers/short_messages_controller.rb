7b33877fe5c1660b9430d38ca79c8ffb5bd5b340
luffycn <kingla_pei@163.com>
2012-03-06 18:17:21 +0800

    Add validation for the message

--- app/controllers/short_messages_controller.rb
+++ app/controllers/short_messages_controller.rb
@@ -2,6 +2,15 @@ class ShortMessagesController < ApplicationController
   def new
     @short_message = ShortMessage.new
   end
+  
+  def create
+    @short_message = ShortMessage.new(params[:short_message])
+    return redirect_to @short_message if @short_message.save
+
+    error = @short_message.errors.first
+	  flash.now[:error] = "#{error[0]} #{error[1]}"
+	  render :action => :new
+  end
 
   def show
   end
--- app/models/short_message.rb
+++ app/models/short_message.rb
@@ -1,2 +1,6 @@
 class ShortMessage < ActiveRecord::Base
+  # --- 校验方法
+  validates :sender_id, :receiver_id, :content, :presence => true
+  validates_length_of :content, :maximum => 500, :message => "不能超过 %d 个字"
+  
 end
